86c13d8b8b8f070649408338c9c6db35c7fb251c
emcog <robin.leonard@gmail.com>
2021-01-07 15:46:19 +0000

    test pending

--- models/cinema.js
+++ models/cinema.js
@@ -4,8 +4,25 @@ const Cinema = function (films) {
 
 
 // list of film titles
-// 
+// we have array of objects
+// we need to access each object in the array
+// we need to acces the title of each object
+// we need to return the title of each object
+// into an array
 
+Cinema.prototype.foundTitles = function() {
+  const titles = this.films.map((film) => {
+    return film.title;
+  });
+
+  return titles;
+}
+
+Cinema.prototype.searchByTitle = function(filmTitle) {
+  const foundFilms = this.films.filter((film) => {
+    return filmTitle === film.title;
+  });
+  return foundFilms[0]; //could use find instead
+};
 
 module.exports = Cinema;
-
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -25,14 +25,30 @@ describe('Cinema', function () {
 
   it('should have a collection of films', function () {
     const actual = cinema.films;
-    console.log(films);
+    // console.log(films);
     assert.deepStrictEqual(actual, films);
   });
+  
+  it('should be able to get a list of film titles', function() {
+    // read throgh MDN array methods and note make of suitable enumration
+    const actual = cinema.foundTitles();
+    filmTitles = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'];
+    assert.deepStrictEqual(actual, filmTitles);
+  });
+  
+  it('should be able to find a film by title', function() {
+    // could have used find method
+    actual = { title: 'Moonlight', genre: 'drama', year: 2016, length: 111 };
+    assert.deepStrictEqual(actual, {title: 'Moonlight', genre: 'drama', year: 2016, length: 111 });
+  });
 
-  xit('should be able to get a list of film titles');
-  xit('should be able to find a film by title');
-  xit('should be able to filter films by genre');
+  it('should be able to filter films by genre'), function() {
+    const actual = [{ title: 'Moonlight', genre: 'drama', year: 2016, length: 111 }, {title: 'T2 Trainspotting', genre: 'drama', year: 2017, length: 117 }]
+    assert.deepStrictEqual(actual, [{ title: 'Moonlight', genre: 'drama', year: 2016, length: 111 }, {title: 'T2 Trainspotting', genre: 'drama', year: 2017, length: 117 }]);
+  };
+  
   xit('should be able to check whether there are some films from a particular year');
+  
   xit('should be able to check whether there are no films from a particular year');
   xit('should be able to check whether all films are over a particular length');
   xit('should be able to calculate total running time of all films');
